8fdff0eff381e0b1c88b806ca69a7a26e9c1d3b8
anandacdr <jazzw815@gmail.com>
2024-05-22 13:20:29 +0545

    05 Conversion

---
+++ 01-Basics/05_conversion_operations.js
@@ -0,0 +1,35 @@
+let score = 34;
+let score1 = "34";
+// let score2 = "ananda";
+let score3 = undefined;
+let score4 = null;
+
+// console.log(typeof (score))
+// console.log(typeof (score1))
+
+
+let valueInNum = Number(score1) // it convert into number
+
+// console.log(typeof (valueInNum))
+
+// console.log(valueInNum)
+
+
+
+
+
+let isLoggedIn = 1
+// let isLoggedIn1 = "AC"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+
+
+let aNumber = 34
+
+let stringNum = String(aNumber)
+
+// console.log(stringNum)
+// console.log(typeof (stringNum))
