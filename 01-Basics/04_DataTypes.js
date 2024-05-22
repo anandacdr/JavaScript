a79b9b165822a3afdc0cc38b54407c1165abbf1e
anandacdr <jazzw815@gmail.com>
2024-05-22 13:05:15 +0545

    data types

--- 01-Basics/04_DataTypes.js
+++ 01-Basics/04_DataTypes.js
@@ -2,3 +2,35 @@
 // Primitive, Non Primitive
 
 
+"use strict"; // it treat all JavaScript Code as newer version
+
+// alert(4+5) // we are using nodejs, not browser
+
+// console.log(34 + 6);
+
+
+// Data Types
+
+let name1 = "ananda";
+let age = 34;
+let state;
+
+// console.log(typeof (name1))
+// console.log(typeof (age))
+
+
+// *** Primitive Data Type ****
+// number
+// bigint
+// string => "Ananda"
+// boolean => true/false
+// null => standalone value
+// undefined => variable is declared, but not defined. i.e. empty
+// symbol => used to find, uniqueness
+
+
+
+// ***** Object ***** //
+
+console.log(typeof "Ananda")
+console.log(typeof null)
