93ef1fa422b4cbc27a41095ae8dd2dca69c3f5a8
anandacdr <jazzw815@gmail.com>
2024-05-26 16:17:21 +0545

    BMI Calculator

---
+++ 06-Projects/02-bmi-calculator/02_script.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('form')
+
+// this usecase will give us empty value
+// const height = parseInt(document.querySelector('#height').value)
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#height').value)
+
+    const weight = parseInt(document.querySelector('#weight').value)
+
+    const results = document.querySelector('#results')
+
+    const bmiGuide = document.querySelector('#bmi-guide')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please, Give a valid height ${height}.`
+    }
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please, Give a valid weight ${weight}.`
+    } else{
+       const bmi =  (weight / ((height*height)/10000)).toFixed(2);
+
+       let classification = '';
+
+       if (bmi < 18.6){
+        classification = 'Under Weight';
+       }
+       else if (bmi >= 18.6 && bmi <= 24.9) {
+        classification = 'Normal Range'
+       }
+       else if (bmi > 24.9) {
+        classification = "Overweight";
+       }
+
+       // show the result
+       results.innerHTML = ` <strong>BMI Level is  <span>${bmi}</span> (${classification})</strong>`;
+    }
+
+    // if (bmi === 18.6) {
+    //     bmiGuide.innerHTML = `Bmi is less than 18.6`
+    // }
+});
