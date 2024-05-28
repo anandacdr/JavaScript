08d9ea98de2089a41a6cdb76f43aa4003f3b79df
anandacdr <jazzw815@gmail.com>
2024-05-28 08:09:16 +0545

    API Request

--- 10 - API Request and v8 Engine/api_request.js
+++ 10 - API Request and v8 Engine/api_request.js
@@ -7,7 +7,7 @@ xhr.onreadystatechange = function () {
   // console.log(xhr.readyState);
   if (xhr.readyState === 4) {
     const data = JSON.parse(this.responseText);
-    // console.log(data);
+    console.log(data);
     // console.log(typeof data);
     // console.log(data.followers);
 
